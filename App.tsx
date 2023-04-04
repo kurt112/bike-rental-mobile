880276d34df8a197f292cf653f71817f51863cff
Kurt Lupin Orioque <korioque@vertere-gs.com>
2023-04-05 07:14:59 +0800

    fix byg

--- App.tsx
+++ App.tsx
@@ -1,66 +1,63 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Login from "./client/screen/auth/Login";
 import customerMainScreen from "./client/screen/customer";
 import RequestNow from "./client/screen/customer/screens/bike/action/request-now";
 import BikeNavigation from "./navigation/Bike";
 import AdminMainScreen from "./client/screen/admin";
 import Register from './client/screen/auth/Register';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
     return (
-        <SafeAreaProvider>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name={'Login'}
-                        component={Login}
-                        options={{
-                            title: 'Login',
-                            navigationBarHidden: true,
-                            headerShown: false
-                        }}
-                    />
-                    <Stack.Screen
-                        name={'Register'}
-                        component={Register}
-                        options={{
-                            title: 'Register'
-                        }}
-                    />
-                    <Stack.Screen name="Customer Screen"
-                        component={customerMainScreen}
-                        options={
-                            {
-                                title: 'Bike Rental',
-                                navigationBarHidden: true,
-                                headerShown: false
-                            }
-                        }
-                    />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name={'Login'}
+                    component={Login}
+                    options={{
+                        title: 'Login',
+                        navigationBarHidden: true,
+                        headerShown: false
+                    }}
+                />
+                 <Stack.Screen
+                    name={'Register'}
+                    component={Register}
+                    options={{
+                        title: 'Register'
+                    }}
+                />
+                <Stack.Screen name="Customer Screen"
+                              component={customerMainScreen}
+                              options={
+                                  {
+                                      title: 'Bike Rental',
+                                      navigationBarHidden: true,
+                                      headerShown: false
+                                  }
+                              }
+                />
 
-                    <Stack.Screen name="Admin Screen"
-                        component={AdminMainScreen}
-                        options={
-                            {
-                                title: 'Admin Screen',
-                                navigationBarHidden: true,
-                                headerShown: false
-                            }
-                        }
-                    />
+                <Stack.Screen name="Admin Screen"
+                              component={AdminMainScreen}
+                              options={
+                                  {
+                                      title: 'Admin Screen',
+                                      navigationBarHidden: true,
+                                      headerShown: false
+                                  }
+                              }
+                />
 
-                    <Stack.Screen name={BikeNavigation.Request.name}
-                        component={RequestNow}
-                        options={{
-                            title: BikeNavigation.Request.title
-                        }}
-                    />
+                <Stack.Screen name={BikeNavigation.Request.name}
+                              component={RequestNow}
+                              options={{
+                                  title: BikeNavigation.Request.title
+                              }}
+                />
 
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaProvider>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
